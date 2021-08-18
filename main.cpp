8ed6dee873fa76fa72fb2fdb0f73c1c19ef3e528
Epic Tazer <78825405+tazer900@users.noreply.github.com>
2021-08-18 01:23:17 +0000

    added way to start

--- main.cpp
+++ main.cpp
@@ -14,5 +14,7 @@ int main() {
   cout << "In this game, there will be several questions that will determine your destiny. So choose wisely." << endl;
   cout << "For each question, you'll get two options: yes & no." << endl;
   cout << "In some special cases though, there will be a 3rd option: I don't know." << endl;
-  cout << "Type in 0 for yes, 1 for no, and 0 for I don't know (if applicable)." << endl;
+  cout << "Type in 0 for yes, 1 for no, and 2 for I don't know (if applicable)." << endl;
+  cout << "Are you ready to start your adventure? Yes - 0, No - 1" << endl;
+  cin >> input;
 }
